0e9349c9db5850fc51db84f3772b68a4526c1e08
shaun <shaun.17g@gmail.com>
2023-09-24 23:11:10 +0800

    Initial Commit

---
+++ challenge-demo/challenge_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_demoApp.swift
+//  challenge-demo
+//
+//  Created by shaun on 2023/9/24.
+//
+
+import SwiftUI
+
+@main
+struct challenge_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
