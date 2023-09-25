cc84aa6a3880e2aab888cea1652d82ebc89d64ce
shaun <shaun.17g@gmail.com>
2023-09-25 10:30:26 +0800

    Initial Commit

---
+++ SwiftUIBeginers/SwiftUIBeginersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeginersApp.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeginersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
