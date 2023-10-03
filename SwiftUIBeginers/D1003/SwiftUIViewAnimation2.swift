d5f94974e8e48372f4c516aa271f27ad92dedc74
shaun <shaun.17g@gmail.com>
2023-10-04 01:45:54 +0800

    11

---
+++ SwiftUIBeginers/D1003/SwiftUIViewAnimation2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewAnimation2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/3.
+//
+
+import SwiftUI
+
+struct SwiftUIViewAnimation2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewAnimation2()
+}
---
+++ SwiftUIBeginers/D1003/SwiftUIViewCornerRotate.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewCornerRotateModifier.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/4.
+//
+
+import SwiftUI
+
+struct SwiftUIViewCornerRotateModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewCornerRotateModifier()
+}
