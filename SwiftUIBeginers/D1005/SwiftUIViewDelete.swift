6dfa88fef149caff3f489d6f48f2b8df1b555621
shaun <shaun.17g@gmail.com>
2023-10-06 22:27:07 +0800

    struct foreach-delete toolbar jsonencoder jsondecoder

---
+++ SwiftUIBeginers/D1005/SwiftUIViewDelete.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewDelete.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/6.
+//
+
+import SwiftUI
+
+struct SwiftUIViewDelete: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewDelete()
+}
---
+++ SwiftUIBeginers/D1005/SwiftUIViewNewView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewNewView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/6.
+//
+
+import SwiftUI
+
+struct SwiftUIViewNewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewNewView()
+}
