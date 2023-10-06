aec4699f0527f240bc68956ac5c820e40597b941
shaun <shaun.17g@gmail.com>
2023-10-06 13:15:25 +0800

    update

---
+++ SwiftUIBeginers/D1005/SwiftUIViewStruct.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/5.
+//
+
+import SwiftUI
+
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
