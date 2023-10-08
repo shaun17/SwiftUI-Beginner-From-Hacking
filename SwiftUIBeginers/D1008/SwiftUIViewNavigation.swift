933d28fa8b99b98c8035aefbffe6a7e5b8a8d1a2
shaun <shaun.17g@gmail.com>
2023-10-08 23:42:40 +0800

    add new view

---
+++ SwiftUIBeginers/D1008/SwiftUIViewNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewNavigation.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/8.
+//
+
+import SwiftUI
+
+struct SwiftUIViewNavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewNavigation()
+}
---
+++ SwiftUIBeginers/D1008/SwiftUIViewScrolleView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewScrolleView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/8.
+//
+
+import SwiftUI
+
+struct SwiftUIViewScrolleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewScrolleView()
+}
