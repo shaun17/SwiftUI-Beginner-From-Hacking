1d6d7f4b65c3fde6436b906e0a90ee90171daafa
shaun <shaun.17g@gmail.com>
2023-10-10 22:20:15 +0800

    do it

---
+++ SwiftUIBeginers/Projects/Project8-domain/SwiftUIViewMissionView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewMissionView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/9.
+//
+
+import SwiftUI
+
+struct SwiftUIViewMissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewMissionView()
+}
