b5c5da5fdb027d2d9383cc47f56293070932e925
shaun <shaun.17g@gmail.com>
2023-10-11 12:18:26 +0800

    commit

---
+++ SwiftUIBeginers/Projects/Project8-domain/AstronautsView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautsView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/10.
+//
+
+import SwiftUI
+
+struct AstronautsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautsView()
+}
