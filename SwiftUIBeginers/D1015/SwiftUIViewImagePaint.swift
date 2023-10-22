95bdcbbd5683c15247a91236f9b10c8957028ffc
shaun <shaun.17g@gmail.com>
2023-10-22 14:10:19 +0800

    feat imgpaint

---
+++ SwiftUIBeginers/D1015/SwiftUIViewImagePaint.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewImagePaint.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/17.
+//
+
+import SwiftUI
+
+struct SwiftUIViewImagePaint: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewImagePaint()
+}
