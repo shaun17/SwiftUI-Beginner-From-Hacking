ad6fed2b76d888d66741ee7fb158c9ccb7846de5
shaun <shaun.17g@gmail.com>
2023-10-24 13:51:22 +0800

    feat: add file for amazing shape

---
+++ SwiftUIBeginers/D1022/SwiftUIViewAmazingSpirograph.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewAmazingSpirograph.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/10/23.
+//
+
+import SwiftUI
+
+struct SwiftUIViewAmazingSpirograph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewAmazingSpirograph()
+}
