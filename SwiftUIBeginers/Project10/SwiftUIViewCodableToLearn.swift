b673bb9549581e4cda454ecffcf4f193098b2ff3
shaun <shaun.17g@gmail.com>
2023-11-05 23:04:30 +0800

    feat:  new feature about codable protocol

---
+++ SwiftUIBeginers/Project10/SwiftUIViewCodableToLearn.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewCodableTo Learn.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/5.
+//
+
+import SwiftUI
+
+struct SwiftUIViewCodableTo_Learn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewCodableTo_Learn()
+}
