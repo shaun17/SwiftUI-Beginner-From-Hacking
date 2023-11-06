4d3326550e00d485150caf270f8fd5e14b032973
shaun <shaun.17g@gmail.com>
2023-11-06 16:42:10 +0800

    freat： progreddView

---
+++ SwiftUIBeginers/Project10/SwiftUIViewAsyncImage.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewAsyncImage.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/6.
+//
+
+import SwiftUI
+
+struct SwiftUIViewAsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewAsyncImage()
+}
