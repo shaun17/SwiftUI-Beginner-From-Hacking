aa141fdd7fed3fa087a2bd454244ccd0478a9811
shaun <shaun.17g@gmail.com>
2023-11-06 16:56:23 +0800

    feat： form disable feature

---
+++ SwiftUIBeginers/Project10/SwiftUIViewFromDisable.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewFromDisable.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/6.
+//
+
+import SwiftUI
+
+struct SwiftUIViewFromDisable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewFromDisable()
+}
