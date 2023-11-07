de39fc705b039302cea606961d09fa48bfbc98a8
shaun <shaun.17g@gmail.com>
2023-11-08 00:01:13 +0800

    feat: add class file

---
+++ SwiftUIBeginers/Project10/Order_Project10.swift
@@ -0,0 +1,8 @@
+//
+//  Order_Project10.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/7.
+//
+
+import Foundation
---
+++ SwiftUIBeginers/Project10/SwiftUIViewOrder.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewOrder.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/7.
+//
+
+import SwiftUI
+
+struct SwiftUIViewOrder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewOrder()
+}
