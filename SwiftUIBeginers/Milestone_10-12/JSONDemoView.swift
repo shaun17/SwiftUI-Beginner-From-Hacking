52271799863af5683ccd3d6b412777b485be4f72
shaun <shaun.17g@gmail.com>
2023-11-23 23:54:32 +0800

    new file

---
+++ SwiftUIBeginers/Milestone_10-12/JSONDemoView.swift
@@ -0,0 +1,18 @@
+//
+//  JSONDemoView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/23.
+//
+
+import SwiftUI
+
+struct JSONDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JSONDemoView()
+}
---
+++ SwiftUIBeginers/Milestone_10-12/ViewBuilderTestView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewBuilderTestView.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2023/11/22.
+//
+
+import SwiftUI
+
+struct ViewBuilderTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewBuilderTestView()
+}
