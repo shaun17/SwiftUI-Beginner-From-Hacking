b5a7a2fc9374e5c990c96655d9e18a3b78b0fb1d
shaun <shaun.17g@gmail.com>
2023-11-23 23:55:00 +0800

    opt: review feature

--- SwiftUIBeginers/Milestone_10-12/JSONDemoView.swift
+++ SwiftUIBeginers/Milestone_10-12/JSONDemoView.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+struct User1: Codable{
+    var firstName: String
+    var lastName: String
+}
+
 struct JSONDemoView: View {
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: method) {
+            Label(
+                title: { /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/ },
+                icon: { Image(systemName: "42.circle") }
+            )
+        }
+    }
+    
+    func method(){
+        let str = """
+        {
+            "first_name": "Andrew",
+            "last_name": "Glouberman"
+        }
+        """
+
+        let data = Data(str.utf8)
+
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+            let user = try decoder.decode(User1.self, from: data)
+            print("Hi, I'm \(user.firstName) \(user.lastName)")
+        } catch {
+            print("Whoops: \(error.localizedDescription)")
+        }
     }
 }
 
--- SwiftUIBeginers/Milestone_10-12/ViewBuilderTestView.swift
+++ SwiftUIBeginers/Milestone_10-12/ViewBuilderTestView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+extension View {
+    
+    
+    func eraseToAnyView() -> AnyView {
+        return AnyView(self)
+    }
+}
 struct ViewBuilderTestView: View {
+    var randomText: some View {
+        
+            if Bool.random() {
+                return Text("Hello, World2!")
+                    .frame(width: 300)
+                    .eraseToAnyView()
+            } else {
+                
+                return Text("Hello, World2!")
+                    .eraseToAnyView()
+                    
+            }
+        }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        randomText
     }
 }
 
