7cb1b978a965e0cafd7129f8581b6f15c0dca10e
shaun <shaun.17g@gmail.com>
2024-03-11 21:41:45 +0800

    第二部分练习

---
+++ SwiftUIBeginers/Project1/DemoPart2.swift
@@ -0,0 +1,18 @@
+//
+//  DemoPart2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/11.
+//
+
+import SwiftUI
+
+struct DemoPart2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemoPart2()
+}
