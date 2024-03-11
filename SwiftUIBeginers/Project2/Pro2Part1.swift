ccc36e87e77fb3341755ffcdca0a67efb106ddce
shaun <shaun.17g@gmail.com>
2024-03-11 23:04:13 +0800

    我想直接提交

---
+++ SwiftUIBeginers/Project2/Pro2Part1.swift
@@ -0,0 +1,18 @@
+//
+//  Pro2Part1.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/11.
+//
+
+import SwiftUI
+
+struct Pro2Part1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pro2Part1()
+}
