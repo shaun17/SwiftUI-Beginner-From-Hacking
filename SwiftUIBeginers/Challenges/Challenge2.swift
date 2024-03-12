463f84abfa42c44305c1febde76b01de828606fc
shaun <shaun.17g@gmail.com>
2024-03-12 15:42:55 +0800

    为什么不能提交？

---
+++ SwiftUIBeginers/Challenges/Challenge2.swift
@@ -0,0 +1,18 @@
+//
+//  Challenge2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/12.
+//
+
+import SwiftUI
+
+struct Challenge2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Challenge2()
+}
