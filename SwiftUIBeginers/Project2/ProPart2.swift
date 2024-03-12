15cf40708b731b404394608e0e843fc263252224
shaun <shaun.17g@gmail.com>
2024-03-12 10:49:24 +0800

    add new file

---
+++ SwiftUIBeginers/Project2/ProPart2.swift
@@ -0,0 +1,18 @@
+//
+//  ProPart2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/12.
+//
+
+import SwiftUI
+
+struct ProPart2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProPart2()
+}
