7d8cde0a644ba1a46d71b0d54ab9086106198df0
shaun <shaun.17g@gmail.com>
2024-03-13 23:50:36 +0800

    add

---
+++ SwiftUIBeginers/Project4/Pro4Part2.swift
@@ -0,0 +1,18 @@
+//
+//  Pro4Part2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/13.
+//
+
+import SwiftUI
+
+struct Pro4Part2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pro4Part2()
+}
---
+++ SwiftUIBeginers/Project5/Pro5Part1.swift
@@ -0,0 +1,18 @@
+//
+//  Pro5Part1.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/13.
+//
+
+import SwiftUI
+
+struct Pro5Part1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pro5Part1()
+}
