354dd31204dac504b9b5447b513a2c2fe109941d
shaun <shaun.17g@gmail.com>
2024-03-15 11:19:36 +0800

    add

---
+++ SwiftUIBeginers/Project5/Pro5Part2.swift
@@ -0,0 +1,18 @@
+//
+//  Pro5Part2.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/3/14.
+//
+
+import SwiftUI
+
+struct Pro5Part2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pro5Part2()
+}
