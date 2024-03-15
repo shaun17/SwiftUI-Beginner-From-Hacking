979005d66f4d0d3f32c8f0420748462332419931
shaun <shaun.17g@gmail.com>
2024-03-15 11:26:11 +0800

    string 文本比较 文本拼接 文本纠错的一些方法

--- SwiftUIBeginers/Project5/Pro5Part1.swift
+++ SwiftUIBeginers/Project5/Pro5Part1.swift
@@ -3,13 +3,56 @@
 //  SwiftUIBeginers
 //
 //  Created by shaun on 2024/3/13.
-//
+// List, Bundle, UITextChecker
 
 import SwiftUI
 
 struct Pro5Part1: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    let input = "a b c"
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(people, id: \.self) {
+            Text($0)
+        }
+
+     
+        ForEach(people, id: \.self){
+            Text("Dynamic row \($0)")
+
+        }
+        .listStyle(.grouped)
+        
+        List(0 ..< 5) {
+            Text("Dynamic row \($0)")
+        }
+        .listStyle(.grouped)
+
+        
+        if let fileUrl = Bundle.main.url(forResource: "", withExtension: "txt"){
+
+        }
+        
+        let letters = input.components(separatedBy: " ")
+        let letter = letters.randomElement()
+
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        Button("check", action: check)
+        
+    }
+    
+    func check(){
+        let word = "swiqft"
+
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
+       print(allGood)
     }
 }
 
--- SwiftUIBeginers/Project5/Pro5Part2.swift
+++ SwiftUIBeginers/Project5/Pro5Part2.swift
@@ -8,8 +8,113 @@
 import SwiftUI
 
 struct Pro5Part2: View {
+    @State private var useWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(useWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit {
+                addNewWord()
+            }
+            .onAppear(perform: {
+                startGame()
+            })
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Next", action: startGame)
+                Button("OK"){}
+
+            } message: {
+                Text(errorMessage)
+            }
+        }
+    }
+    
+    func addNewWord() {
+        let input = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        if input.count < 1 { return }
+        
+        guard isOriginal(word: input) else {
+            wordError(title: "Word used already", message: "Be more original")
+                return
+        }
+        guard isPossible(word: input) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+
+            return
+        }
+        
+        guard isReal(word: input) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+
+            return
+        }
+        withAnimation {
+            useWords.insert(input, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let start = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: start) {
+                let words = startWords.components(separatedBy: "\n")
+                rootWord = words.randomElement() ?? "N/A"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !useWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var temp = rootWord
+        for letter in word {
+            if let pos = temp.firstIndex(of: letter) {
+                temp.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let chekcer = UITextChecker()
+        let range = NSRange(location: 0, length: word.count)
+        let misspell = chekcer.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspell.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
