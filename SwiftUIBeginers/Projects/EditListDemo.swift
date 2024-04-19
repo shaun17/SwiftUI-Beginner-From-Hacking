4f240cd589f152daa227f2ff0ae40759d3cb6f31
shaun <shaun.17g@gmail.com>
2024-04-19 22:45:21 +0800

    list的editButton测试，初始化edit module状态，确保第一次点击就能生效

---
+++ SwiftUIBeginers/Projects/EditListDemo.swift
@@ -0,0 +1,18 @@
+//
+//  EditListDemo.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/4/19.
+//
+
+import SwiftUI
+
+struct EditListDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditListDemo()
+}
