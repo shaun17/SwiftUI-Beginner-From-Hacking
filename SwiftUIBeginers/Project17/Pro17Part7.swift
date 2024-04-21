9233449c8fe7c48c821ef5867720113a049e0406
shaun <shaun.17g@gmail.com>
2024-04-21 18:40:46 +0800

    使用uuid的方案 改写了一下

---
+++ SwiftUIBeginers/Project17/Pro17Part7.swift
@@ -0,0 +1,18 @@
+//
+//  Pro17Part7.swift
+//  SwiftUIBeginers
+//
+//  Created by shaun on 2024/4/21.
+//
+
+import SwiftUI
+
+struct Pro17Part7: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pro17Part7()
+}
